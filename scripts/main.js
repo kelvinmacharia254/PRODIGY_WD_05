92ea27f63c4665bcfa69bc9898b5ca8f5ceb1053
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-09-30 09:31:07 +0300

    generilize function to use city or coordinates for fetch

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,5 @@
-// async function fetchWeatherInfo(){
-//     const API_KEY = "237ec7ebf9e55cae7d76169e622669ea"
-//     const {latitude, longitude} = await getLocation();
-//     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
-//
-//     if (!response.ok) {
-//         const error = new Error("Failed to fetch weather info");
-//         throw error;
-//     }
-//
-//     const weatherInfo = await response.json();
-//     return weatherInfo;
-// }
-//
-// (async () => {
-//     try {
-//         const weatherData = await fetchWeatherInfo();
-//         console.log(weatherData); // Log the weather information
-//     } catch (error) {
-//         console.error(error.message); // Log any errors
-//     }
-// })();
-
 // GUI elements
-//  Display
+//  Display elements
 const iconElement = document.querySelector('.weather-icon');
 const locationIcon = document.querySelector('.location-icon');
 const tempElement = document.querySelector('.temperature-value');
@@ -30,9 +7,8 @@ const descElement = document.querySelector('.temperature-description p');
 const locationElement = document.querySelector('.location p');
 const notificationElement = document.querySelector('.notification');
 
-//  inputs
-let input = document.querySelector('#search');
-
+//  inputs elements
+let searchInput = document.querySelector('#search');
 
 // Global variables
 let city;
@@ -48,6 +24,7 @@ const KELVIN = 273
 
 const apiKey = '237ec7ebf9e55cae7d76169e622669ea'  // https://home.openweathermap.org/api_keys
 
+
 // fetch current location
 if("geolocation" in navigator){
     navigator.geolocation.getCurrentPosition(setPosition, showError)
@@ -61,7 +38,8 @@ function setPosition(position){
     latitude = position.coords.latitude
     longitude = position.coords.longitude
     console.log(latitude, longitude)
-    getWeather(latitude, longitude)
+    // get weather of current location on loading
+    getWeatherData("coords")
 }
 
 function showError(error){
@@ -69,72 +47,62 @@ function showError(error){
 }
 
 
-function getWeather(latitude, longitude) {
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+async function callWeatherAPI(location){
+    let apiUrl
+    if(location === "coords"){
+        apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+    }else if(location === "city"){
+        apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    }
 
-    fetch(apiUrl)
-        .then(response=>{
-            let data = response.json()
-            console.log(`data => ${data}`)
-            return data
-        }).then(data=>{
-            weather.temparature.value = Math.floor(data.main.temp - KELVIN)
-            weather.description = data.weather[0].description
-            weather.iconId = data.weather[0].icon
-            weather.city = data.name
-            weather.country = data.sys.country
-            console.log(`weather = ${weather}`)
-        }).then( () => {
-        }).then( () => {
-            displayweather()
-        })
+    const response = await fetch(apiUrl)
+
+    if (!response.ok) {
+        const error = new Error("Failed to fetch weather info");
+        throw error;
+    }
+
+    const weatherInfo = await response.json();
+    return weatherInfo;
 }
 
 
-function getSearchWeather(city){
-    let api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+async function getWeatherData (location) {
+    try {
+        const weatherData = await callWeatherAPI(location);
+        console.log(weatherData); // Log the weather information
+        weather.temparature.value = Math.floor(weatherData.main.temp - KELVIN)
+        weather.description = weatherData.weather[0].description
+        weather.iconId = weatherData.weather[0].icon
+        weather.city = weatherData.name
+        weather.country = weatherData.sys.country
+        displayweather()
+        console.log(`weather = ${weather}`)
+    } catch (error) {
+        console.error(error.message); // Log any errors
+    }
+};
 
-   fetch(api)
-        .then(response=>{
-            let data = response.json()
-            console.log(`data => ${data}`)
-            return data
-        }).then(data=>{
-            weather.temparature.value = Math.floor(data.main.temp - KELVIN)
-            weather.description = data.weather[0].description
-            weather.iconId = data.weather[0].icon
-            weather.city = data.name
-            weather.country = data.sys.country
-            console.log(`weather = ${weather}`)
-        }).then( () => {
-        }).then( () => {
-            displayweather()
-        })
-}
 
 function displayweather(){
-
+    searchInput.value = weather.city
     iconElement.innerHTML = `<img src="https://openweathermap.org/img/wn/${weather.iconId}@2x.png">`
     // iconElement.innerHTML = `<img src="icons/${weather.iconId}.png">`
-    tempElement.innerHTML = `${weather.temparature.value}*<span>C</span>`
+    tempElement.innerHTML = `${weather.temparature.value}<sup>o</sup><span>C</span>`
     descElement.innerHTML = weather.description
     locationElement.innerHTML = `${weather.city}, ${weather.country}\n [${latitude}, ${longitude}]`
 }
 
-
-
-input.addEventListener('keyup', event => {
+searchInput.addEventListener('keyup', event => {
     if (event.keyCode ===13){
         event.preventDefault()
 
-        city=input.value;
-        getSearchWeather(city);
-        console.log(city);
+        city=searchInput.value;
+        getWeatherData("city");
     }
 });
 
 
 locationIcon.addEventListener("click", event => {
-    console.log("Hey")
-    getWeather()
+    getWeatherData("coords");
 })
